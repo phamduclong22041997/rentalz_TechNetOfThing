d1ead673e26e7366dd11c58265f88e05cf8319dd
Long Pham <long.pham@eton.vn>
2021-12-03 00:03:18 +0700

    -a

---
+++ Trentalz_Android/app/src/main/java/com/app/rentalz_TechNetOfThing/utils/OnSelectListener.java
@@ -0,0 +1,7 @@
+package com.app.rentalz_TechNetOfThing.utils;
+
+import android.widget.DatePicker;
+
+public interface OnSelectListener {
+    void onSelectDate(DatePicker view, int year, int month, int day);
+}
